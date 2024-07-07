ab0bcaf295bdc06777bd2b0a2f97637fe7aa9528
petarstankov <stankov.pg@gmail.com>
2024-07-07 20:46:13 +0300

    latest updates

---
+++ books-context/src/components/BookEdit.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import useBooksContext from '../hooks/useBooksContext';
+
+function BookEdit({ book, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+  const { editBooksById } = useBooksContext();
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit();
+    editBooksById(book.id, title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
+}
+
+export default BookEdit;
